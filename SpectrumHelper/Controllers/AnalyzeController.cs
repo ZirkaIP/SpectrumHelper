dab90b06fcb736b31a71634ea2c8f1e1039336ec
Ilia <i.pulman.official@gmail.com>
2021-03-31 11:23:59 +0300

    initial commit

---
+++ SpectrumHelper/Controllers/AnalyzeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpectrumHelper.ViewModels;
+
+namespace SpectrumHelper.Controllers
+{
+	public class AnalyzeController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+
+		public IActionResult SendToAnalyze(IncomingParameters parameterss)
+		{
+			return View(Index());
+		}
+	}
+}
---
+++ SpectrumHelper/ViewModels/IncomingParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpectrumHelper.ViewModels
+{
+	public class IncomingParameters
+	{
+		[Display (Name = "Нижняя частота")]
+		[Required (ErrorMessage = "Поле не может быть пустым")]
+		public decimal LowerFrequency { get; set; }
+
+		[Display (Name = "Верхняя частота")]
+		[Required(ErrorMessage = "Поле не может быть пустым")]
+		public decimal UpperFrequency { get; set; }
+	}
+}
